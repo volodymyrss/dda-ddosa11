30b8d837e0abf6376adaf0c4535c4fd9f10d9ad8
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2017-01-31 01:30:24 +0100

    ii_shadow_build

--- ddosa11.py
+++ ddosa11.py
@@ -81,6 +81,8 @@ class ibis_comp_energy(DataAnalysis):
 
         self.output_events=DataFile("compton_events_corrected.fits")
 
+#/sps/integral/data/ic/ic_snapshot_20140321/ic/ibis/mod/isgr_effc_mod_0001.fits
+
 class GetEcorrCalDB(DataAnalysis):
     input=["ecorr_standard_OSA10.2"]
     input_lut2=GetLUT2
@@ -97,4 +99,127 @@ class GetEcorrCalDB(DataAnalysis):
         self.risedol=self.input_ibisic.ibisicroot+"/mod/isgr_rise_mod_0239.fits"
         self.mcecdol=self.input_ibisic.ibisicroot+"/mod/isgr_mcec_mod_0001.fits"
         self.l2redol=self.input_ibisic.ibisicroot+"/mod/isgr_l2re_mod_0001.fits"
+
+
+class BinEventsVirtual(DataAnalysis):
+    input_scw=ScWData
+
+    input_ibisic=IBIS_ICRoot
+
+    input_events=ISGRIEvents
+    input_gti=ibis_gti
+    input_dead=ibis_dead
+
+    target_level=None
+    input_bins=None
+
+    maxrisetime=116
+    minrisetime=16
+
+    version="v3"
+    
+    cached=True
+
+    default_log_level="binevents"
+
+    ii_shadow_build_binary="ii_shadow_build"
+
+
+    def get_version(self):
+        v=self.get_signature()+"."+self.version
+        if self.maxrisetime!=116:
+            v+=".lrt%i"%self.maxrisetime
+        if self.minrisetime!=16:
+            v+=".hrt%i"%self.minrisetime
+        return v
+
+    def main(self):
+        if self.target_level is None or self.input_bins is None:
+            raise Exception("VirtualAnalysis: please inherit!")
+        
+        self.pre_process()
+
+        # ask stephane why need raw events
+        construct_gnrl_scwg_grp(self.input_scw,[\
+            self.input_events.events.get_path(), \
+            self.input_scw.scwpath+"/isgri_events.fits[3]", \
+            self.input_scw.scwpath+"/ibis_hk.fits[IBIS-DPE.-CNV]", \
+            self.input_scw.auxadppath+"/time_correlation.fits[AUXL-TCOR-HIS]" \
+        ]) # get separately tc etc
+
+        import_attr(self.input_scw.scwpath+"/swg.fits",['OBTSTART','OBTEND'])
+        set_attr({'ISDCLEVL':self.target_level})
+
+        det_fn="isgri_detector_shadowgram_%s.fits"%self.target_level
+        det_tpl="(ISGR-DETE-SHD-IDX.tpl)"
+        eff_fn="isgri_efficiency_shadowgram_%s.fits"%self.target_level
+        eff_tpl="(ISGR-EFFI-SHD-IDX.tpl)"
+
+        remove_withtemplate(det_fn+det_tpl)
+        remove_withtemplate(eff_fn+eff_tpl)
+
+        bin=self.ii_shadow_build_binary
+        ht=heatool(bin)
+        ht['outSWGGRP']="og.fits[GROUPING,1,BINTABLE]"
+        ht['inDead']=self.input_dead.output_dead.get_path()
+        ht['inGTI']=self.input_gti.output_gti.get_path()
+        ht['gti_name'] = 'MERGED_ISGRI'
+        ht['outputLevel'] = self.target_level
+
+        print "target_level",self.target_level
+
+        print "has rmfbins "+str(self.input_bins.rmfbins) if hasattr(self.input_bins,'rmfbins') else "no rmfbins"
+        print "has binrmfext" if hasattr(self.input_bins,'binrmfext') else "no binrmfext"
+
+        if ( self.target_level=="BIN_I" or not hasattr(self.input_bins,'rmfbins') or not self.input_bins.rmfbins or not hasattr(self.input_bins,'binrmfext') ) and not ( hasattr(self.input_bins,'rmfbins') and self.input_bins.rmfbins ): # fix!!
+            ht['isgri_e_num'] = len(self.input_bins.bins)
+            ht['isgri_e_min'] = " ".join([str(a[0]) for a in self.input_bins.bins])
+            ht['isgri_e_max'] = " ".join([str(a[1]) for a in self.input_bins.bins])
+        elif self.target_level=="BIN_S" or ( hasattr(self.input_bins,'rmfbins') and self.input_bins.rmfbins ) or hasattr(self.input_bins,'binrmfext'):
+            ht['isgri_e_num'] = -1
+
+            rmf=self.input_bins.binrmfext
+            if isinstance(rmf,DataFile):
+                ht['inEnergyValues'] = rmf.get_path()  #  +"[1]" will this work?
+            else:
+                ht['inEnergyValues'] = rmf #  +"[1]" will this work?
+        else:
+            raise Exception("neither bins given!")
+
+        ht['isgri_min_rise'] = self.minrisetime
+        ht['isgri_max_rise'] = self.maxrisetime
+        ht['isgri_t_len'] = 10000000
+        ht['idxLowThre']=self.input_scw.revdirpath+"/idx/isgri_context_index.fits[1]"
+        ht['idxNoisy']=self.input_scw.revdirpath+"/idx/isgri_prp_noise_index.fits[1]"
+        ht['outRawShadow']=det_fn+det_tpl
+        ht['outEffShadow']=eff_fn+eff_tpl
+        ht['inEFFC']=self.input_ibisic.ibisicroot+"/mod/isgr_effc_mod_0001.fits"
+
+        self.extra_pars(ht)
+
+        ht.run()
+
+        self.shadow_detector=DataFile(det_fn)
+        self.shadow_efficiency=DataFile(eff_fn)
+        
+        self.post_process()
+
+    def extra_pars(selt,ht):
+        pass
+
+    def post_process(self):
+        pass
+    
+    def pre_process(self):
+        pass
+
+
+
+class BinEventsImage(BinEventsVirtual):
+    target_level="BIN_I"
+    input_bins=ImageBins
+
+class BinEventsSpectra(BinEventsVirtual):
+    target_level="BIN_S"
+    input_bins=SpectraBins
 
