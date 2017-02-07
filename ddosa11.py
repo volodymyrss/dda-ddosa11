3b3c3f671bce31c8f06708c18c473d318d6b21b6
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-02-07 23:22:46 +0100

    update

--- ddosa11.py
+++ ddosa11.py
@@ -224,3 +224,19 @@ class BinEventsSpectra(BinEventsVirtual):
     target_level="BIN_S"
     input_bins=SpectraBins
 
+class SpectraBins(DataAnalysis):
+    input_binsname="spectral_bins_256"
+    
+    rmfbins=True
+
+    version="v1"
+    def main(self):
+        self.binrmf=os.environ['CURRENT_IC']+"/ic/ibis/mod/isgr_ebds_mod_0001.fits"
+        e=pyfits.open(self.binrmf)[1].data
+        self.bins=zip(e['E_MIN'],e['E_MAX'])
+        self.binrmfext=self.binrmf+'[1]'
+
+    def get_binrmfext(self):
+        return self.binrmfext
+
+
